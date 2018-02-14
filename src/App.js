39e4dbe8ff16599ecdbb029a4ab97b1064dfb841
rocketkevin <kevinchen91200@gmail.com>
2018-02-14 23:29:34 +0000

    Updata

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import DayPage from "./DayPage";
+import WeekPage from "./WeekPage";
 
 class App extends Component {
   render() {
@@ -13,7 +16,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Route path = "/today" component = {DayPage}/>
+        <Route exact path = "/" component = {WeekPage}/>
       </div>
+      
     );
   }
 }
